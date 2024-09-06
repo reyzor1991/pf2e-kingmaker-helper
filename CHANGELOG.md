f1886eb67cca70dd1a87c14ec2abc2f72f8b609f
Mykhailo Budiakov <mishabudjakov@gmail.com>
2024-09-07 00:17:10 +0300

    - Show campsite icons for crit rolls

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## Version 0.1.1
+- Show campsite icons for crit rolls
+
 ## Version 0.1.0
 - Show resources icons
 - Show features icons, up to 9 icons on hex
--- src/main.js
+++ src/main.js
@@ -1,5 +1,61 @@
 const moduleName = "pf2e-kingmaker-helper";
 
+class CampsiteActivities extends foundry.abstract.DataModel {
+
+    static defineSchema() {
+        const fields = foundry.data.fields;
+        return {
+            data: new fields.ArrayField(new fields.SchemaField({
+                key: new fields.NumberField({initial: 0}),
+                result: new fields.StringField({initial: ""})
+            }))
+        };
+    }
+
+    static load() {
+        const data = game.settings.get(moduleName, "data");
+        return this.fromSource(data);
+    }
+
+    async update() {
+        await game.settings.set(moduleName, "data", this.toObject());
+    }
+}
+
+const OUTCOME_TO_KEY = {
+    "failure": "f_campfire",
+    "success": "s_campfire",
+    "criticalFailure": "cf_campfire",
+    "criticalSuccess": "cs_campfire",
+}
+
+const ADDITIONAL_ACTIVITIES = Object.freeze({
+    'cs_campfire': {
+        id: "cs_campsite",
+        img: `modules/${moduleName}/assets/cs_campfire.webp`,
+        label: "pf2e-kingmaker-helper.FEATURES.cs_campsite",
+        hint: "You find the perfect spot for a camp. Flat checks to determine encounters at the campsite for the next 24 hours have a DC 2 higher than normal, and the first hour spent performing Camping activities does not incur the usual flat check for random encounters."
+    },
+    's_campfire': {
+        id: "s_campsite",
+        img: `modules/${moduleName}/assets/s_campfire.webp`,
+        label: "pf2e-kingmaker-helper.FEATURES.s_campsite",
+        "hint": "You find a serviceable spot for a camp and for Camping activities."
+    },
+    'cf_campfire': {
+        id: "cf_campsite",
+        img: `modules/${moduleName}/assets/cf_campfire.webp`,
+        label: "pf2e-kingmaker-helper.FEATURES.cf_campsite",
+        hint: "Your campsite will work, but it's not the best. Campsite activities that require checks take a -2 penalty."
+    },
+    'f_campfire': {
+        id: "f_campsite",
+        img: `modules/${moduleName}/assets/f_campfire.webp`,
+        label: "pf2e-kingmaker-helper.FEATURES.f_campsite",
+        hint: "The campsite is a mess. You can use it to rest and to perform daily preparations, but it isn't good enough to allow for Campsite activities at all. Worse, your attempt to secure a campsite has possibly attracted unwanted attention-attempt a flat check against the zone's Encounter DC. If successful, a random encounter automatically occurs."
+    }
+});
+
 const normalize = (val, max, min) => {
     return (val - min) / (max - min);
 };
@@ -10,10 +66,10 @@ const hexToAlpha = (alphaHexString) => {
 
 function hexes(filter) {
     return Object.entries(foundry.utils.deepClone(game.settings.get("pf2e-kingmaker", "state").hexes))
-        .filter((data)=>filter(data[1]))
-        .map(a=>Number(a[0]))
-        .map(n=>kingmaker.region.hexes.get(n))
-        .filter(h=>!!h);
+        .filter((data) => filter(data[1]))
+        .map(a => Number(a[0]))
+        .map(n => kingmaker.region.hexes.get(n))
+        .filter(h => !!h);
 }
 
 Hooks.once('ready', () => {
@@ -32,15 +88,17 @@ function fillColor(g, polygons, color, alpha) {
     g.endFill();
 }
 
-function createPolygon(hexes) {
-    return hexes.map(h=> {
-        let v = canvas.grid.getVertices(h);
-        for (let i of v) {
-            i.x += h.center.x
-            i.y += (h.center.y-158)
-        }
-        return new PIXI.Polygon(v)
-    })
+function createPolygon(hex) {
+    const v = canvas.grid.getVertices(hex);
+    for (let i of v) {
+        i.x += hex.center.x
+        i.y += (hex.center.y - 158)
+    }
+    return new PIXI.Polygon(v)
+}
+
+function createPolygons(hexes) {
+    return hexes.map(h => createPolygon(h))
 }
 
 function getColor(name) {
@@ -111,6 +169,14 @@ Hooks.on('init', function () {
             game.coloredAndIconsLayer?.draw()
         }
     });
+
+    game.settings.register(moduleName, "data", {
+        scope: "world",
+        config: false,
+        requiresReload: false,
+        type: CampsiteActivities,
+        default: {}
+    });
 })
 
 Hooks.on("getSceneControlButtons", buttons => {
@@ -141,25 +207,30 @@ class ColoredAndIconsLayer extends PIXI.Container {
 
     draw() {
         this.children.forEach(c => c.destroy());
-        if (!this.visible) {return}
+        if (!this.visible) {
+            return
+        }
         let g = this.addChild(new PIXI.Graphics());
         this.#drawColored(g)
     }
 
     #drawColored(g) {
-        this.#drawColorByType(g, h=>h.exploration === 1 && !h.claimed, getColor("reconnoiteredHexColor"));
-        this.#drawColorByType(g, h=>h.exploration === 2 && !h.claimed, getColor("mappedHexColor"));
-        this.#drawColorByType(g, h=>h.claimed, getColor("claimedHexColor"));
+        this.#drawColorByType(g, h => h.exploration === 1 && !h.claimed, getColor("reconnoiteredHexColor"));
+        this.#drawColorByType(g, h => h.exploration === 2 && !h.claimed, getColor("mappedHexColor"));
+        this.#drawColorByType(g, h => h.claimed, getColor("claimedHexColor"));
         this.#drawResourceIcon(g)
     }
 
     #drawColorByType(g, filter, color) {
-        fillColor(g, createPolygon(hexes(filter)), color.color, color.alpha)
+        fillColor(g, createPolygons(hexes(filter)), color.color, color.alpha)
     }
 
     #drawResourceIcon(graphics) {
         let all = hexes(h => !!h.commodity && h.showResources);
-        all = [...all, ...hexes(h=>h.features && h.features.length > 0), ...hexes(h=>h.camp)]
+        all = [...all, ...hexes(h => h.features && h.features.length > 0), ...hexes(h => h.camp)]
+
+
+        let additionalData = CampsiteActivities.load().data;
 
         let scaledSize = ORIGIN_ICON_SIZE * SCALE;
         let correction = scaledSize / 2
@@ -173,26 +244,23 @@ class ColoredAndIconsLayer extends PIXI.Container {
                 let image = new PIXI.Sprite(
                     PIXI.Texture.from(kingmaker.CONST.COMMODITIES[hex.data.commodity].img)
                 );
-                image.scale.set(SCALE, SCALE)
-                let x = hex.center.x - correction;
-                let y = hex.center.y - correction;
-                x += (MAP_MOVEMENT[position].x * scaledSize)
-                y += (MAP_MOVEMENT[position].y * scaledSize)
-                image.position.set(x, y)
-                graphics.addChild(image);
+                setImage(image, graphics, hex, correction, position, scaledSize)
                 position += 1;
             }
             if (hex.data.camp && hex.data.showResources) {
                 let image = new PIXI.Sprite(
                     PIXI.Texture.from(kingmaker.CONST.CAMPS[hex.data.camp].img)
                 );
-                image.scale.set(SCALE, SCALE)
-                let x = hex.center.x - correction;
-                let y = hex.center.y - correction;
-                x += (MAP_MOVEMENT[position].x * scaledSize)
-                y += (MAP_MOVEMENT[position].y * scaledSize)
-                image.position.set(x, y)
-                graphics.addChild(image);
+                setImage(image, graphics, hex, correction, position, scaledSize)
+                position += 1;
+            }
+
+            let createCampsite = additionalData.find(ad => ad.key === hex.key);
+            if (createCampsite) {
+                let image = new PIXI.Sprite(
+                    PIXI.Texture.from(ADDITIONAL_ACTIVITIES[createCampsite.result].img)
+                );
+                setImage(image, graphics, hex, correction, position, scaledSize)
                 position += 1;
             }
 
@@ -205,19 +273,22 @@ class ColoredAndIconsLayer extends PIXI.Container {
                 let image = PIXI.Sprite.from(
                     kingmaker.CONST.FEATURES[feature.type].img
                 );
-                image.scale.set(SCALE, SCALE)
-                let x = hex.center.x - correction;
-                let y = hex.center.y - correction;
-                x += (MAP_MOVEMENT[position].x * scaledSize)
-                y += (MAP_MOVEMENT[position].y * scaledSize)
-
-                image.position.set(x, y)
-                graphics.addChild(image);
+                setImage(image, graphics, hex, correction, position, scaledSize)
                 position += 1;
             }
             position = 0;
         }
     }
+}
+
+function setImage(image, graphics, hex, correction, position, scaledSize) {
+    image.scale.set(SCALE, SCALE)
+    let x = hex.center.x - correction;
+    let y = hex.center.y - correction;
+    x += (MAP_MOVEMENT[position].x * scaledSize)
+    y += (MAP_MOVEMENT[position].y * scaledSize)
+    image.position.set(x, y)
+    graphics.addChild(image);
 }
 
 Hooks.on("canvasReady", () => {
@@ -230,4 +301,72 @@ Hooks.on("updateSetting", (setting) => {
     if (setting.key === 'pf2e-kingmaker.state') {
         game.coloredAndIconsLayer?.draw()
     }
+});
+
+Hooks.on("renderKingmakerHexEdit", (form, html, params) => {
+    if (form.object.key) {
+        let cAct = CampsiteActivities.load();
+
+        let data = cAct.data;
+        let find = data.find(d => d.key === form.object.key);
+        if (!find) {
+            return
+        }
+        let additionalActivity = ADDITIONAL_ACTIVITIES[find.result];
+        let newHtml = `
+            <fieldset>
+                <div class="form-group">
+                    <label data-tooltip="${additionalActivity.hint}" aria-label="">${game.i18n.localize(additionalActivity.label)}</label>
+                    <button type="button" class="icon fa-solid fa-times remove-aa" style="flex: 0.1;"></button>
+                </div>
+            </fieldset>
+        `
+        html.find('footer').before(newHtml);
+
+        html.on("click", '.remove-aa', async (_e, a) => {
+            $(_e.target).closest('fieldset').remove();
+            cAct.updateSource({
+                data: data.filter(d => d.key !== form.object.key)
+            })
+            await cAct.update()
+            game.coloredAndIconsLayer?.draw()
+        });
+    }
+});
+
+Hooks.on("createChatMessage", async (message) => {
+    if (game.user !== game.users.activeGM) {
+        return
+    }
+    if (!message?.flags?.pf2e?.context?.options?.includes("action:prepare-campsite")) {
+        return;
+    }
+    if (!["criticalFailure", "criticalSuccess"].includes(message.flags.pf2e.context.outcome)) {
+        return;
+    }
+
+    let token = game.actors.party.getActiveTokens(true, true)[0]
+    if (!token) {return;}
+
+    let hex = kingmaker.region.hexes
+        .map(h=>[createPolygon(h), h])
+        .filter(hh=>hh[0].contains(token.center.x, token.center.y))
+        .map(hh=>hh[1])[0]
+    if (!hex) {return;}
+
+    let cA = CampsiteActivities.load();
+    let data = cA.data;
+    let find = data.find(d => d.key === hex.key);
+    if (find) {
+        find.result = OUTCOME_TO_KEY[message.flags.pf2e.context.outcome]
+        cA.updateSource({data})
+    } else {
+        data.push({
+            key: hex.key,
+            result: OUTCOME_TO_KEY[message.flags.pf2e.context.outcome]
+        })
+        cA.updateSource({data})
+    }
+    await cA.update()
+    game.coloredAndIconsLayer?.draw()
 });
